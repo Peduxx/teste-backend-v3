4e268dd6feff1c431766db8533ead8dcf5b50cab
Peduxx <phalves944@gmail.com>
2025-03-26 13:55:32 -0300

    add infrastructure layer

---
+++ TheatricalPlayersRefactoringKata.Infrastructure/Factories/OutputTypeFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using TheatricalPlayersRefactoringKata.Domain.Abstractions;
+using TheatricalPlayersRefactoringKata.Domain.Enums;
+using TheatricalPlayersRefactoringKata.Domain.Generators;
+
+namespace TheatricalPlayersRefactoringKata.Infrastructure.Factories
+{
+    public class OutputTypeFactory
+    {
+        public static IOutputGenerator CreateOutputType(EOutputType outputType, CultureInfo cultureInfo)
+        {
+            return outputType switch
+            {
+                EOutputType.Text => new TextOutputGenerator(cultureInfo),
+                EOutputType.XML => new XMLOutputGenerator(cultureInfo),
+                _ => throw new ArgumentException("Invalid output type."),
+            };
+        }
+    }
+}
---
+++ TheatricalPlayersRefactoringKata.Infrastructure/Factories/PlayTypeFactory.cs
@@ -0,0 +1,20 @@
+﻿using TheatricalPlayersRefactoringKata.Domain.Enums;
+using TheatricalPlayersRefactoringKata.Domain.Primitives;
+using TheatricalPlayersRefactoringKata.Domain.Types.PlayTypes;
+
+namespace TheatricalPlayersRefactoringKata.Infrastructure.Factories
+{
+    public class PlayTypeFactory
+    {
+        public static PlayType CreatePlayType(EPlayType playtype)
+        {
+            return playtype switch
+            {
+                EPlayType.Comedy => new ComedyType(),
+                EPlayType.Tragedy => new TragedyType(),
+                EPlayType.Historical => new HistoricalType(),
+                _ => throw new ArgumentException("Invalid playtype."),
+            };
+        }
+    }
+}
